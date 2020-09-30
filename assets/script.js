ff38bfb9a04d049c6685c132a225ed01270bb225
Conanas <tmhaigh@gmail.com>
2020-09-30 11:47:43 +1000

    updated project htmls to backticks to look nicer

--- assets/script.js
+++ assets/script.js
@@ -30,40 +30,40 @@ function loadWeatherDashboardProject() {
     // What makes the project interesting technically
     // Brief description of the project
     // A list of languages used in the application
-    modalBodyText.html(
-        "<h4>Description</h4>" +
+    modalBodyText.html(`
+        <h4>Description</h4>
 
-        "<p>" +
+        <p>
+            Planning a round the world trip? Then this web 
+            app helps you plan that trip by giving you a 5 
+            day forecast of any chosen city on your route. 
+            Weather Dashboard will save your chosen cities 
+            for your future reference.
+        </p>
+        <p>
+            Weather Dashboard is an excellent example of 
+            using third party web API's. The app calls weather
+            information from openweathermap.org then using 
+            JavaScript and JQuery the returned information 
+            is rendered onto a single html page, using css 
+            for styling and bootstrap for responsiveness.
+        </p>
 
-        "Planning a round the world trip? Then this web app helps you plan that trip by giving you a " +
-        "5 day forecast of any chosen city on your route. Weather Dashboard will save your chosen cities " +
-        "for your future reference." +
+        <h4>Technologies used</h4>
 
-        "</p>" +
+        <ul>
+            <li>html</il>
+            <li>css</il>
+            <li>Bootstrap</il>
+            <li>javaScript</il>
+            <li>jQuery</il>
+            <li>Third Party Web API</il>
+            <ul>
+                <li>openweathermap.org</il>
+            </ul>
+        </ul>
 
-        "<p>" +
-
-        "Weather Dashboard is an excellent example of using third party web API's. The app calls weather " +
-        "information from openweathermap.org then using JavaScript and JQuery the returned information " +
-        "is rendered onto a single html page, using css for styling and bootstrap for responsiveness" +
-
-        "</p>" +
-
-        "<h4>Technologies used</h4>" +
-
-        "<ul>" +
-        "<li>html</il>" +
-        "<li>css</il>" +
-        "<li>Bootstrap</il>" +
-        "<li>javaScript</il>" +
-        "<li>jQuery</il>" +
-        "<li>Third Party Web API</il>" +
-        "<ul>" +
-        "<li>openweathermap.org</il>" +
-        "</ul>" +
-        "</ul>"
-
-    );
+    `);
     liveLink.attr("href", "https://conanas.github.io/weather-dashboard/");
     githubLink.attr("href", "https://github.com/Conanas/weather-dashboard");
 }
@@ -82,35 +82,39 @@ function loadWorkSchedulerProject() {
         src: "./assets/screenshots/work-day-scheduler/work-scheduler-change-time.png",
         alt: "work-scheduler-items-added - copy"
     });
-    modalBodyText.html(
-        "<h4>Description</h4>" +
+    modalBodyText.html(`
+        <h4>Description</h4>
 
-        "<p>" +
+        <p>
+            Need to organise your work day? This web app makes 
+            planning your work day easy. Start your day with an 
+            empty plan, then fill in the time slots with events 
+            for the day. Work Day Scheduler will save the events 
+            if you need to refresh your browser and helps keep 
+            track of the time by changing the colours of the 
+            time slots based on the current time of the day.
+        </p>
+        <p>
+            Work Day Scheduler is a fine example of using 
+            localStorage to save and retrieve information. 
+            Moment.js is utilised to retrieve the current 
+            time of day which is then used by the javaScript 
+            to render the background colours of the different 
+            timeslots. App is fully responsive thanks to bootstrap 
+            and the styling on the html is achieved by css.
+        </p>
 
-        "Need to organise your work day? This web app makes planning your work day easy. Start your day with an empty " +
-        "plan, then fill in the time slots with events for the day. Work Day Scheduler will save the events if you need to refresh " +
-        "your browser and helps keep track of the time by changing the colours of the time slots based on the current time of the day." +
+        <h4>Technologies used</h4>
 
-        "</p>" +
-        "<p>" +
-
-        "Work Day Scheduler is a fine example of using localStorage to save and retrieve information. Moment.js is utilised to " +
-        "retrieve the current time of day which is then used by the javaScript to render the background colours of the different " +
-        "timeslots. App is fully responsive thanks to bootstrap and the styling on the html is achieved by css." +
-
-        "</p>" +
-
-        "<h4>Technologies used</h4>" +
-
-        "<ul>" +
-        "<li>html</il>" +
-        "<li>css</il>" +
-        "<li>Bootstrap</il>" +
-        "<li>javaScript</il>" +
-        "<li>jQuery</il>" +
-        "<li>moment.js</il>" +
-        "</ul>"
-    );
+        <ul>
+            <li>html</il>
+            <li>css</il>
+            <li>Bootstrap</il>
+            <li>javaScript</il>
+            <li>jQuery</il>
+            <li>moment.js</il>
+        </ul>
+    `);
     liveLink.attr("href", "https://conanas.github.io/work-day-scheduler/");
     githubLink.attr("href", "https://github.com/Conanas/work-day-scheduler");
 }
@@ -129,33 +133,35 @@ function loadTobyTimeProject() {
         src: "./assets/screenshots/toby-time/toby-screenshots-face.png",
         alt: "toby-screenshots-face"
     });
-    modalBodyText.html(
-        "<h4>Description</h4>" +
+    modalBodyText.html(`
+        <h4>Description</h4>
 
-        "<p>" +
+        <p>
+            Like to workout? Like Cats? Toby Time is a set and 
+            rep tracker for your workouts and upon completion 
+            you are rewarded with a picture of my cat Toby. 
+            Simply enter the reps, rest time, sets and break 
+            time and start doing your pressups, pullups or 
+            situps etc... You can save your workouts parameters 
+            to local storage to be reloaded for future use.
+        </p>
+        <p>
+            Fully responsive with the use of bootstrap, a single 
+            html page is styled with CSS and rendered with javaScript 
+            and jQuery. Includes the use of sounds to indicate when 
+            the rest or break period is about to end.
+        </p>
 
-        "Like to workout? Like Cats? Toby Time is a set and rep tracker for your workouts and upon completion you are rewarded " +
-        "with a picture of my cat Toby. Simply enter the reps, rest time, sets and break time and start doing your pressups, pullups " +
-        "or situps etc... You can save your workouts parameters to local storage to be reloaded for future use." +
+        <h4>Technologies used</h4>
 
-        "</p>" +
-        "<p>" +
-
-        "Fully responsive with the use of bootstrap, a single html page is styled with CSS and rendered with javaScript and jQuery. " +
-        "Includes the use of sounds to indicate when the rest or break period is about to end." +
-
-        "</p>" +
-
-        "<h4>Technologies used</h4>" +
-
-        "<ul>" +
-        "<li>html</il>" +
-        "<li>css</il>" +
-        "<li>Bootstrap</il>" +
-        "<li>javaScript</il>" +
-        "<li>jQuery</il>" +
-        "</ul>"
-    );
+        <ul>
+            <li>html</il>
+            <li>css</il>
+            <li>Bootstrap</il>
+            <li>javaScript</il>
+            <li>jQuery</il>
+        </ul>
+    `);
     liveLink.attr("href", "https://conanas.github.io/toby-time/");
     githubLink.attr("href", "https://github.com/Conanas/toby-time");
 }
@@ -174,40 +180,44 @@ function loadWeatherWearProject() {
         src: "./assets/screenshots/weather-wear/weather-wear-welcom-screen.png",
         alt: "weather-wear-welcom-screen"
     });
-    modalBodyText.html(
-        "<h4>Description</h4>" +
+    modalBodyText.html(`
+        <h4>Description</h4>
 
-        "<p>" +
+        <p>
+            WeatherWear is a single paged, fully responsive javaScript 
+            and jQuery rendered html web app styled with CSS that aids 
+            users plan what to wear depending on the weather forecast 
+            for a user selected time range. The user is presented with 
+            going out and coming home times that are generated live 
+            with the use of moment.js. After the location and times 
+            are selected WeatherWear will show the user what they are 
+            suggested to wear based on the current temperature, uv 
+            index and wind speed.
+        </p>
+        <p>
+            As well as suggesting what users should wear WeatherWear 
+            has functionality for users to look up local places to 
+            eat whilst they are out. The generated places-to-eat list 
+            can be filtered by cuisine type and sorted by rating and 
+            by price.
+        </p>
 
-        "WeatherWear is a single paged, fully responsive javaScript and jQuery rendered html web app styled with CSS that aids " +
-        "users plan what to wear depending on the weather forecast for a user selected time range. The user is presented " +
-        "with going out and coming home times that are generated live with the use of moment.js. After the location and times are selected " +
-        "WeatherWear will show the user what they are suggested to wear based on the current temperature, uv index and wind speed." +
+        <h4>Technologies used</h4>
 
-        "</p>" +
-        "<p>" +
-
-        "As well as suggesting what users should wear WeatherWear has functionality for users to look up local places to eat whilst they are out. " +
-        "The generated places-to-eat list can be filtered by cuisine type and sorted by rating and by price." +
-
-        "</p>" +
-
-        "<h4>Technologies used</h4>" +
-
-        "<ul>" +
-        "<li>html</il>" +
-        "<li>css</il>" +
-        "<li>Materialize</il>" +
-        "<li>javaScript</il>" +
-        "<li>jQuery</il>" +
-        "<li>moment.js</il>" +
-        "<li>Third Party API's</il>" +
-        "<ul>" +
-        "<li>weatherbit.io</il>" +
-        "<li>zomato.com</il>" +
-        "</ul>" +
-        "</ul>"
-    );
+        <ul>
+            <li>html</il>
+            <li>css</il>
+            <li>Materialize</il>
+            <li>javaScript</il>
+            <li>jQuery</il>
+            <li>moment.js</il>
+            <li>Third Party API's</il>
+            <ul>
+                <li>weatherbit.io</il>
+                <li>zomato.com</il>
+            </ul>
+        </ul>
+    `);
     liveLink.attr("href", "https://lionhatt.github.io/project-WeatherWear/");
     githubLink.attr("href", "https://github.com/lionhatt/project-WeatherWear");
 }
