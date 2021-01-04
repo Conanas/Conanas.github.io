83fa9ab92f1aff63152936392e0b92f8c589f004
Tom Haigh <tmhaigh@gmail.com>
2021-01-05 10:02:30 +1100

    moved footer and header jsx to components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import Header from './components/Header/';
+import Footer from './components/Footer/';
 import './App.css';
 import weatherWearImage from "./assets/screenshots/weather-wear/weather-wear-welcome-screen.png";
 import workDaySchedulerImage from "./assets/screenshots/work-day-scheduler/work-scheduler-main.png";
@@ -11,49 +13,7 @@ import portfolioImage from "./assets/portfolio-img/tom-portfolio-photo.jpg"
 function App() {
   return (
     <>
-      <header id="home-container">
-        <div className="container">
-          <div className="row">
-            <section id="home-section" className="page-section col">
-
-              <h1>Tom Haigh Portfolio Page</h1>
-
-              <p className="section-statement">
-                Full-stack web developer with JavaScript, HTML, CSS, React.js, MySQL, PHP, Web API and responsive web design skills dedicated to making the web a more accessible and user friendly experience. Projects include a Weather Dashboard, a Work Day Scheduler,
-                a workout timer and a web app that helps you decide what to wear when heading out.
-            </p>
-              <div className="home-contact-info">
-                <label id="phone-number" className="contact-info">
-                  Phone: 0406 253 030 ||
-              </label>
-                <label id="email-address" className="contact-info">
-                  Email: tmhaigh@gmail.com ||
-                </label>
-                <label className="contact-info">
-                  Location: Melbourne, Victoria, Australia
-              </label>
-              </div>
-              <a id="resume-button" href="https://drive.google.com/file/d/1z3gjUkCu5Dn3i_F7ltW7irqJMeowSegt/view?usp=sharing" target="_blank" rel="noreferrer">
-                <i className="fas fa-external-link-alt"></i> Resume
-            </a>
-              <div id="links-div">
-                <a className="links-div-anchor" href="https://www.linkedin.com/in/thomas-haigh-5960b81aa/" target="_blank" rel="noreferrer">
-                  <i className="fab fa-linkedin font-awesome social-icon"></i>
-                </a>
-                <a className="links-div-anchor" href="https://github.com/Conanas" target="_blank" rel="noreferrer">
-                  <i className="fab fa-github font-awesome social-icon"></i>
-                </a>
-              </div>
-
-              <a href="#about-section-target">
-                <input id="portfolio-button" type="button" value="View my Portfolio" />
-              </a>
-
-            </section>
-          </div>
-        </div>
-
-      </header>
+      <Header />
 
       <nav className="navbar sticky-top navbar-expand bg-dark navbar-dark">
 
@@ -239,26 +199,7 @@ function App() {
 
       </main>
 
-      <footer id="footer-container">
-        {/* <!-- Your name -->
-        <!-- Basic contact information -->
-        <!-- Links to your social media (at least LinkedIn and GitHub) -->
-        <!-- Your location (City, State) -->
-        <!-- A link to your resume --> */}
-        <div>
-          <label id="phone-number" className="phone-number">Phone: 0406 253 030 || </label>
-          <label id="email-address" className="email-address">Email: tmhaigh@gmail.com || </label>
-          <label>Location: Melbourne, Victoria, Australia</label>
-        </div>
-        <div>
-          <a className="links-div-anchor" href="https://www.linkedin.com/in/thomas-haigh-5960b81aa/" target="_blank" rel="noreferrer">
-            <i className="fab fa-linkedin font-awesome social-icon"></i>
-          </a>
-          <a className="links-div-anchor" href="https://github.com/Conanas" target="_blank" rel="noreferrer">
-            <i className="fab fa-github font-awesome social-icon"></i>
-          </a>
-        </div>
-      </footer>
+      <Footer />
 
       {/* <!-- Modal --> */}
       <div id="project-modal" className="modal fade" role="dialog">
