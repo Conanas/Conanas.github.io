7d296ce1a4970bcc50fd9aac8a0ce02731d1a5a6
Tom Haigh <tmhaigh@gmail.com>
2021-01-05 10:11:47 +1100

    moved about jsx to components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header from './components/Header/';
+import About from './components/About/';
 import Footer from './components/Footer/';
 import './App.css';
 import weatherWearImage from "./assets/screenshots/weather-wear/weather-wear-welcome-screen.png";
@@ -8,7 +9,6 @@ import eatDatBurgerImage from "./assets/screenshots/eat-dat-burger/eat-dat-burge
 import mealBudgeterImage from "./assets/screenshots/meal-budgeter/meal-budgeter-screenshot.png";
 import noteTakerImage from "./assets/screenshots/note-taker/note-taker.png";
 import tobyTimeImage from "./assets/screenshots/toby-time/toby-screenshot-main.png";
-import portfolioImage from "./assets/portfolio-img/tom-portfolio-photo.jpg"
 
 function App() {
   return (
@@ -43,35 +43,7 @@ function App() {
 
       <main>
 
-        <div id="about-container">
-          <div className="container">
-            <div className="row">
-              <section id="about-section" className="page-section col">
-                <a className="about-target-anchor" name="about-section-target">&nbsp;</a>
-                {/* <!-- About -->
-                        <!-- Who am I? -->
-                        <!-- List of soft skills that differentiate you from others -->
-                        <!-- A link to your resume --> */}
-                <h1>About</h1>
-                <div className="row about-main-div">
-                  <div className="col">
-                    <img className="float-left" id="about-img" src={portfolioImage} alt="portfolio" />
-                    <p className="section-statement">
-                      Originally from the Peak District National Park in the UK, I came to Australia in 2010 and I became an Australian citizen in 2018. It is in the Peak District I got into rock climbing and I continue to climb because not only do I enjoy moving on rock but
-                      because of the similarities rock climbing has with coding. They both require a lot of problem solving and give me great satisfaction when I complete a climb or a project.
-                                </p>
-                    <p className="section-statement">
-                      I am a resourceful full-stack web developer based in Melbourne, Australia with creative problem solving skills who is an adaptable team player that values effective communication, hard work and self improvement through critical feedback.
-                                </p>
-                  </div>
-                </div>
-                <div>
-
-                </div>
-              </section>
-            </div>
-          </div>
-        </div>
+        <About />
 
         <div id="portfolio-container">
           <div className="container">
