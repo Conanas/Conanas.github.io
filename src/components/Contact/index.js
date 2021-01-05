64aa5bbb0c21f736932f9cf0ef3dd2b2282e5224
Tom Haigh <tmhaigh@gmail.com>
2021-01-05 12:22:13 +1100

    moved contact section to components

---
+++ src/components/Contact/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './style.css';
+
+function Contact() {
+  return (
+    <div id="contact-container">
+      <div className="container">
+        <div className="row">
+          <section id="contact-section" className="page-section col">
+            <a className="target-anchor" name="contact-section-target">&nbsp;</a>
+            {/* <!-- Contact --> */}
+            {/* <!-- Set up email messaging form --> */}
+            <h1>Contact</h1>
+            <form action="mailto:someone@example.com" method="post" enctype="text/plain">
+              <label className="contact-label" for="name"> Name:</label><br />
+              <input className="contact-input" type="text" name="name" /><br />
+
+              <label className="contact-label" for="mail">E-mail:</label><br />
+              <input className="contact-input" type="text" name="mail" /><br />
+
+              <label className="contact-label" for="comment">Comment:</label><br />
+              <textarea name="comment" id="contact-textarea" rows="10"></textarea>
+
+              <br /><br />
+
+              <input className="contact-button" type="submit" value="Send" />
+              <input className="contact-button" type="reset" value="Reset" />
+            </form>
+          </section>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Contact
