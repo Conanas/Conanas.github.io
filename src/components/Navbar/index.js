1a95677dada872f88cd3a08318562625d41c3772
Tom Haigh <tmhaigh@gmail.com>
2021-01-06 15:14:37 +1100

    removed navbar dark theme

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,7 +3,7 @@ import './style.css';
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-md bg-dark navbar-dark">
+    <nav className="navbar navbar-expand-md navbar-dark">
       <div className="container">
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
