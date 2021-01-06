ae55b4eab3f5ab0166f916502eae8cd181286254
Tom Haigh <tmhaigh@gmail.com>
2021-01-07 10:27:14 +1100

    updated Modal with prop inpuct

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,24 +1,39 @@
 import React from 'react';
 import './style.css';
 
-function Modal() {
+function Modal({ product }) {
   return (
     < div id="project-modal" className="modal fade" role="dialog" >
       <div className="modal-dialog">
         <div className="modal-content">
           <div className="modal-header">
-            <h4 className="modal-title"></h4>
+            <h4 className="modal-title">{product.title}</h4>
             <button type="button" className="close" data-dismiss="modal">&times;</button>
           </div>
           <div className="modal-body">
             <div>
-              <img id="modal-image" src="" alt="" />
+              <img id="modal-image" src={product.image} alt={product.name} />
             </div>
-            <div id="project-description"></div>
-            <a id="live-link" href="" target="_blank">
+            <div id="project-description">
+              <h4>Description</h4>
+              {product.description.map(item => {
+                return (
+                  <p>{item}</p>
+                )
+              })}
+              <h4>Technologies used</h4>
+              <ul>
+                {product.technologies.map(item => {
+                  return (
+                    <li>{item}</li>
+                  )
+                })}
+              </ul>
+            </div>
+            <a href={product.live_link} target="_blank" rel="noreferrer">
               <input className="modal-link-button" type="button" value="View Live Project" />
             </a>
-            <a id="github-link" href="" target="_blank">
+            <a href={product.github_repo} target="_blank" rel="noreferrer">
               <input className="modal-link-button" type="button" value="View GitHub Repository" />
             </a>
           </div>
