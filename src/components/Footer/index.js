4e36e7814f1d9dfb3f2d6c1998c81f7c51282fb5
Tom Haigh <tmhaigh@gmail.com>
2021-01-07 08:57:20 +1100

    moved resume link to social div icons

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,7 +6,10 @@ function Footer() {
     <footer>
       <div className="container">
         <div className="row d-flex flex-sm-row-reverse">
-          <div className="col-sm-6 d-flex justify-content-sm-end align-items-sm-center">
+          <div className="col-sm-6 d-flex justify-content-sm-end align-items-center">
+            <a className="links-div-anchor" href="https://drive.google.com/file/d/1z3gjUkCu5Dn3i_F7ltW7irqJMeowSegt/view?usp=sharing" target="_blank" rel="noreferrer">
+              <i className="fas fa-external-link-alt"></i> Resume
+            </a>
             <a className="links-div-anchor" href="https://www.linkedin.com/in/thomas-haigh-5960b81aa/" target="_blank" rel="noreferrer">
               <i className="fab fa-linkedin font-awesome social-icon"></i>
             </a>
@@ -15,11 +18,6 @@ function Footer() {
             </a>
           </div>
           <div className="col-sm-6 d-flex flex-column">
-            <label>
-              <a href="https://drive.google.com/file/d/1z3gjUkCu5Dn3i_F7ltW7irqJMeowSegt/view?usp=sharing" target="_blank" rel="noreferrer">
-                <i className="fas fa-external-link-alt"></i> Resume
-              </a>
-            </label>
             <label>Phone: 0406 253 030</label>
             <label>Email: tmhaigh@gmail.com</label>
             <label>Location: Melbourne, Victoria, Australia</label>
