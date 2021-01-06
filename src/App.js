c261e0c04ff547d00ec827125500b1f73b9d994a
Tom Haigh <tmhaigh@gmail.com>
2021-01-06 11:16:03 +1100

    added routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from './components/Home/';
 import Header from './components/Header/';
-import Navbar from './components/Navbar/';
 import About from './components/About/';
 import Projects from './components/Projects/';
 import Contact from './components/Contact/';
@@ -9,17 +11,29 @@ import './App.css';
 
 function App() {
   return (
-    <>
+    <div className="wrapper">
       <Header />
-      <Navbar />
       <main>
-        <About />
-        <Projects />
-        <Contact />
+        <Router>
+          <Switch>
+            <Route exact path='/'>
+              <Home />
+            </Route>
+            <Route exact path='/about'>
+              <About />
+            </Route>
+            <Route exact path='/products'>
+              <Projects />
+            </Route>
+            <Route exact path='/contact'>
+              <Contact />
+            </Route>
+          </Switch>
+        </Router>
       </main>
       <Footer />
       <Modal />
-    </>
+    </div>
   );
 }
 
