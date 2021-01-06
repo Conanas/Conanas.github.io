e762730feca983f87fb846657fe6cd0e37349b3d
Tom Haigh <tmhaigh@gmail.com>
2021-01-07 10:27:58 +1100

    added Modal import and initial state coding

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -1,30 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./style.css";
 import ProjectsData from '../../assets/data/projects.json';
+import Modal from '../Modal/';
 
 
 export default function Products() {
+  // const [product, updateProduct] = useState();
+
+  // const handleClick = (project) => {
+  //   updateProduct(project);
+  //   console.log(product)
+  // }
+
   return (
-    <div className="container">
-      <div className="row">
-        <section className="col">
-          <h1>Products</h1>
-          <div className="card-deck d-flex flex-lg-row flex-column">
-            {ProjectsData.map(project => {
-              return (
-                <div className="card">
-                  <img className="card-img-top" src={project.image} alt={project.name} />
-                  <div className="card-body d-flex flex-column">
-                    <h5 className="card-title">{project.title}</h5>
-                    <p className="card-text">{project.quick_description}</p>
-                    <button className="btn" id={`${project.name}-button`} data-toggle="modal" data-target="#project-modal">View Project Info</button>
+    <>
+      <div className="container">
+        <div className="row">
+          <section className="col">
+            <h1>Products</h1>
+            <div className="card-deck d-flex flex-lg-row flex-column">
+              {ProjectsData.map(project => {
+                return (
+                  <div className="card">
+                    <img className="card-img-top" src={project.image} alt={project.name} />
+                    <div className="card-body d-flex flex-column">
+                      <h5 className="card-title">{project.title}</h5>
+                      <p className="card-text">{project.quick_description}</p>
+                      <button
+                        className="btn"
+                        id={`${project.name}-button`}
+                        data-toggle="modal"
+                        data-target="#project-modal"
+                      // onClick={handleClick(project)}
+                      >
+                        View Project Info
+                      </button>
+                    </div>
                   </div>
-                </div>
-              )
-            })}
-          </div>
-        </section>
+                )
+              })}
+            </div>
+          </section>
+        </div>
       </div>
-    </div>
+      <Modal product={ProjectsData[0]} />
+    </>
   )
 }
