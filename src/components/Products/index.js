cfae41bef03ef0cd0705c2dcc2295f406bec7b9c
Tom Haigh <tmhaigh@gmail.com>
2021-01-06 15:15:16 +1100

    changed portfolio to products

---
+++ src/components/Products/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import "./style.css";
+import ProjectsData from '../../assets/data/projects.json';
+
+
+export default function Products() {
+  return (
+    <div className="container products-section">
+      <div className="row">
+        <section className="col">
+          <h1>Products</h1>
+          <div className="card-deck d-flex flex-lg-row flex-column">
+            {ProjectsData.map(project => {
+              return (
+                <div className="card">
+                  <img className="card-img-top" src={project.image} alt={project.name} />
+                  <div className="card-body d-flex flex-column">
+                    <h5 className="card-title">{project.title}</h5>
+                    <p className="card-text">{project.quick_description}</p>
+                    <button className="btn" id={`${project.name}-button`} data-toggle="modal" data-target="#project-modal">View Project Info</button>
+                  </div>
+                </div>
+              )
+            })}
+          </div>
+        </section>
+      </div>
+    </div>
+  )
+}
