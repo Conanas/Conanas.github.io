02ad7048dcb5eeb3e89447c3157315f29329263c
Tom Haigh <tmhaigh@gmail.com>
2021-01-06 11:17:24 +1100

    created home section with brand statement

---
+++ src/components/Home/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './style.css';
+
+export default function Home() {
+  return (
+    <section className="home-section container">
+      <div className="row">
+        <div className="col">
+          <h1>Thomas Haigh</h1>
+          <p className="brand-statement">
+            Full-stack web developer with JavaScript, HTML, CSS, React.js, Express.js,
+            Node.js, MongoDB, MySQL, ORM, Web API and responsive web design skills
+            dedicated to making the web a more accessible and user friendly experience.
+            A retail management background has developed my strengths in problem solving,
+            teamwork, meeting deadlines and successfully grew a retail store project
+            into a profitable asset. Obtained a degree in Computer Science at the University
+            of Nottingham giving a solid understanding of coding fundamentals and the Monash
+            University Full Stack Coding Bootcamp has built my skills in web development.
+            Led a team of four to develop a product that helps users to save money on food
+            and cooking during the Covid-19 2020 lockdown in Victoria with a recipe budgeting
+            application. I am motivated to contribute my full-stack web skills and computer
+            science background to build better experiences on the web in a fast paced
+            environment with a professional team.
+          </p>
+        </div>
+      </div>
+    </section>
+  )
+}
