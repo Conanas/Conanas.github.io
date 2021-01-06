d7a765cc4fd4022313a143c4fc5f3fafbbaf56cc
Tom Haigh <tmhaigh@gmail.com>
2021-01-06 16:28:43 +1100

    removed redundant classes

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -3,29 +3,26 @@ import './style.css';
 
 function Contact() {
   return (
-    <div id="contact-container">
-      <div className="container">
-        <div className="row">
-          <section id="contact-section" className="page-section col">
-            <a className="target-anchor" name="contact-section-target">&nbsp;</a>
-            <h1>Contact</h1>
-            <form action="mailto:someone@example.com" method="post" enctype="text/plain">
-              <label className="contact-label" for="name"> Name:</label><br />
-              <input className="contact-input" type="text" name="name" /><br />
+    <div className="container contact-section">
+      <div className="row">
+        <section className="col">
+          <h1>Contact</h1>
+          <form action="mailto:someone@example.com" method="post" enctype="text/plain">
+            <label className="contact-label" for="name"> Name:</label><br />
+            <input className="contact-input" type="text" name="name" /><br />
 
-              <label className="contact-label" for="mail">E-mail:</label><br />
-              <input className="contact-input" type="text" name="mail" /><br />
+            <label className="contact-label" for="mail">E-mail:</label><br />
+            <input className="contact-input" type="text" name="mail" /><br />
 
-              <label className="contact-label" for="comment">Comment:</label><br />
-              <textarea name="comment" id="contact-textarea" rows="10"></textarea>
+            <label className="contact-label" for="comment">Comment:</label><br />
+            <textarea name="comment" id="contact-textarea" rows="10"></textarea>
 
-              <br /><br />
+            <br /><br />
 
-              <input className="contact-button" type="submit" value="Send" />
-              <input className="contact-button" type="reset" value="Reset" />
-            </form>
-          </section>
-        </div>
+            <input className="contact-button" type="submit" value="Send" />
+            <input className="contact-button" type="reset" value="Reset" />
+          </form>
+        </section>
       </div>
     </div>
   )
