73089dcade43ea5d5c4008c64cfad6669b1317ea
Tom Haigh <tmhaigh@gmail.com>
2021-01-08 18:21:49 +1100

    changed animation times

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -8,19 +8,19 @@ function Contact() {
         <section className="col">
           <h1 className="section-heading">Contact</h1>
           <form action="mailto:someone@example.com" method="post" encType="text/plain">
-            <label className="contact-label" htmlFor="name"> Name:</label><br />
-            <input className="contact-input" type="text" name="name" /><br />
+            <label className="contact-label name-label" htmlFor="name"> Name:</label><br />
+            <input className="contact-input name-input" type="text" name="name" /><br />
 
-            <label className="contact-label" htmlFor="mail">E-mail:</label><br />
-            <input className="contact-input" type="text" name="mail" /><br />
+            <label className="contact-label email-label" htmlFor="mail">E-mail:</label><br />
+            <input className="contact-input email-input" type="text" name="mail" /><br />
 
-            <label className="contact-label" htmlFor="comment">Comment:</label><br />
-            <textarea name="comment" id="contact-textarea" rows="6"></textarea>
+            <label className="contact-label comment-label" htmlFor="comment">Comment:</label><br />
+            <textarea className="comment-input" name="comment" id="contact-textarea" rows="6"></textarea>
 
             <br /><br />
 
-            <input className="contact-button" type="submit" value="Send" />
-            <input className="contact-button" type="reset" value="Reset" />
+            <input className="contact-button submit-button" type="submit" value="Send" />
+            <input className="contact-button reset-button" type="reset" value="Reset" />
           </form>
         </section>
       </div>
