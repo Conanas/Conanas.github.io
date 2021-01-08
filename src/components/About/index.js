02526409cf6b54b2c92905adfd0e1d619c4c0949
Tom Haigh <tmhaigh@gmail.com>
2021-01-08 17:44:31 +1100

    added heading classname to h1 put statement in div

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,15 +7,17 @@ function About() {
     <div className="container">
       <div className="row">
         <section className="col">
-          <h1>About</h1>
+          <h1 className="section-heading">About</h1>
           <img className="float-left" id="about-img" src={portfolioImage} alt="portfolio" />
-          <p>
-            Originally from the Peak District National Park in the UK, I came to Australia in 2010 and I became an Australian citizen in 2018. It is in the Peak District I got into rock climbing and I continue to climb because not only do I enjoy moving on rock but
-            because of the similarities rock climbing has with coding. They both require a lot of problem solving and give me great satisfaction when I complete a climb or a project.
-            </p>
-          <p>
-            I am a resourceful full-stack web developer based in Melbourne, Australia with creative problem solving skills who is an adaptable team player that values effective communication, hard work and self improvement through critical feedback.
-            </p>
+          <div className="about-statement">
+            <p>
+              Originally from the Peak District National Park in the UK, I came to Australia in 2010 and I became an Australian citizen in 2018. It is in the Peak District I got into rock climbing and I continue to climb because not only do I enjoy moving on rock but
+              because of the similarities rock climbing has with coding. They both require a lot of problem solving and give me great satisfaction when I complete a climb or a project.
+          </p>
+            <p>
+              I am a resourceful full-stack web developer based in Melbourne, Australia with creative problem solving skills who is an adaptable team player that values effective communication, hard work and self improvement through critical feedback.
+          </p>
+          </div>
         </section>
       </div>
     </div>
