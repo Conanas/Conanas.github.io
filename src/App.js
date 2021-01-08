9802bfad3326bcd561696dac0d9337956b0cf336
Tom Haigh <tmhaigh@gmail.com>
2021-01-08 14:54:31 +1100

    changed basename to /

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <div className="wrapper">
       <Header />
       <main className="d-flex align-items-center">
-        <Router basename={''}>
+        <Router basename={'/'}>
           <Switch>
             <Route exact path='/'>
               <Home />
