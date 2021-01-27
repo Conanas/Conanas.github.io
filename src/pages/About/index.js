ac6b26b7589a50dac260ea5f78539b49f75bd364
Tom Haigh <tmhaigh@gmail.com>
2021-01-28 10:33:20 +1100

    inserted animated heading and statement

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Heading from '../../components/Heading/';
+import Statement from '../../components/Statement/';
 import "./style.css";
 import portfolioImage from "../../assets/portfolio-img/tom-portfolio-photo.jpg";
 
@@ -7,17 +9,17 @@ function About() {
     <div className="container">
       <div className="row">
         <section className="col">
-          <h1 className="section-heading">About</h1>
+          <Heading>About</Heading>
           <img className="float-left" id="about-img" src={portfolioImage} alt="portfolio" />
-          <div className="about-statement">
+          <Statement className="about-statement">
             <p>
               Originally from the Peak District National Park in the UK, I came to Australia in 2010 and I became an Australian citizen in 2018. It is in the Peak District I got into rock climbing and I continue to climb because not only do I enjoy moving on rock but
               because of the similarities rock climbing has with coding. They both require a lot of problem solving and give me great satisfaction when I complete a climb or a project.
-          </p>
+            </p>
             <p>
               I am a resourceful full-stack web developer based in Melbourne, Australia with creative problem solving skills who is an adaptable team player that values effective communication, hard work and self improvement through critical feedback.
-          </p>
-          </div>
+            </p>
+          </Statement>
         </section>
       </div>
     </div>
