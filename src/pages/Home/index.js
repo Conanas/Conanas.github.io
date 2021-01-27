eb1e3b497fe0d05ec3125618cb478110f96e964a
Tom Haigh <tmhaigh@gmail.com>
2021-01-28 10:33:05 +1100

    removed style.css

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Heading from '../../components/Heading/';
 import Statement from '../../components/Statement/';
-import './style.css';
 
 export default function Home() {
   return (
